2a16ae34dfeba9355c768c754e2881fd2ed04d03
Vinicius-E <viniciuseduardo0702@hotmail.com>
2021-08-29 18:06:29 -0300

    JPA respository, dependency injection, database seeding

---
+++ src/main/java/com/udemy/SpringBoot01/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.udemy.SpringBoot01.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.udemy.SpringBoot01.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
